98eb2e69853a23cd2a25a41e723e1cfe53137999
laravel <dqdev204@gmail.com>
2024-07-26 16:46:20 +0700

    init project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+
+    public function showFormlogin(Request $request)
+    {
+        return view("auth.login");
+    }
+    public function login(Request $request)
+    {
+
+        $user = $request->validate([
+            'email' => 'required|string|max:255',
+            'password' => 'required|string',
+        ]); // check password database
+        // dd($user);
+        if (Auth::attempt($user)) { // kiem tra neu dung thi ban ve home
+            return redirect()->intended('admins/dashboard');
+        }
+
+        return redirect()->back()->withErrors(['email' => 'Thông tin người dùng không đúng !']);
+    }
+
+
+    public function showFormRegister(Request $request)
+    {
+
+        return view("auth.register");
+    }
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+        $user = User::query()->create($data);
+        Auth::login($user);
+        return redirect()->intended('home');
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Http\Controllers\Admin\DanhMucController;
+use App\Http\Controllers\Admin\SanPhamController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Clients\checkout;
+use App\Http\Controllers\Clients\test;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('login', [AuthController::class, 'showFormlogin']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::get('register', [AuthController::class, 'showFormRegister']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::get('/home', function () {
+//     return view('home');
+// })->middleware('auth');
+
+// Route::get('/admin', function () {
+//     return "Đây là trang admin";
+// })->middleware(['auth', 'auth.admin']);
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/home', function () {
+//         return view('home');
+//     });
+
+//     Route::middleware('auth.admin')->group(function () {
+//         Route::get('/admin', function () {
+//             return "Đây là trang admin !";
+//         });
+//     });
+
+// });
+
+Route::middleware(['auth', 'auth.admin'])->prefix('admins')
+    ->as('admins.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admins.dashboard');
+        })->name('dashboard');
+
+        Route::prefix('danhmucs')
+            ->as('danhmucs.')
+            ->group(function () {
+                Route::get('/', [DanhMucController::class, 'index'])->name('index');
+                Route::get('/create', [DanhMucController::class, 'create'])->name('create');
+                Route::post('/store', [DanhMucController::class, 'store'])->name('store');
+                Route::get('/show/{id}', [DanhMucController::class, 'show'])->name('show');
+                Route::get('{id}/edit', [DanhMucController::class, 'edit'])->name('edit');
+                Route::put('{id}/update', [DanhMucController::class, 'update'])->name('update');
+                Route::delete('{id}/destroy', [DanhMucController::class, 'destroy'])->name('destroy');
+            });
+        Route::prefix('sanphams')
+            ->as('sanphams.')
+            ->group(function () {
+                Route::get('/', [SanPhamController::class, 'index'])->name('index');
+                Route::get('/create', [SanPhamController::class, 'create'])->name('create');
+                Route::post('/store', [SanPhamController::class, 'store'])->name('store');
+                Route::get('/show/{id}', [SanPhamController::class, 'show'])->name('show');
+                Route::get('{id}/edit', [SanPhamController::class, 'edit'])->name('edit');
+                Route::put('{id}/update', [SanPhamController::class, 'update'])->name('update');
+                Route::delete('{id}/destroy', [SanPhamController::class, 'destroy'])->name('destroy');
+            });
+    });
